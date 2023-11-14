cf389346ad1eafe3dec331ccdb6354803b6531ef
Tyler <tyleralamo15@gmail.com>
2023-11-14 11:32:55 -0800

    Skeleton of the Fridge and Item classes - Tyler

---
+++ refrigeratorClasses.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Fridge {
+    public:
+    void putItem();
+    void removeItem();
+    void addSection();
+
+    void setPassword();
+    void setUsername();
+    void setUser();
+    void setSection();
+
+    double getVolume();
+    string getUsername();
+    string getPassword();
+    vector<string> getContents();
+    vector<string> getUsers();
+    vector<string> getSections();
+
+    private:
+    struct section; // This will be how we can store the different sizes of the sections of the fridge (l,w,d)
+    vector<section> sections; //This will hold all of the different sections of the fridge
+    vector<string> users;
+    vector<Item> items;
+    string username;
+    string password;
+    string name;
+    double volume;
+
+};
+
+
+class Item {
+    public:
+    void setName();
+    void setVolume(); // This will ask the user for the items measurements (l,w,d)
+    void setExpiration();
+
+    string getName();
+    string getExpiration();
+    double getVolume();
+
+    private:
+    string name;
+    string expiration;
+    double volume;
+
+    // Allows for Fridge object to access private members in 'Item' class
+    friend class Fridge;
+};
+
