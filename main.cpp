ee4f04397707e895991bfb4461788fd4f41690a1
kelsonnguyen <116989424+kelsonnguyen@users.noreply.github.com>
2023-11-29 11:42:48 -0800

    notification of allergy will now be in main

--- main.cpp
+++ main.cpp
@@ -11,12 +11,29 @@ int main()
 
     Fridge kitchenMasterTest = fridges.getFridges()[0];
     
+    string addedItemName = "Shellfish";
+    string sectionName = "Middle Shelf";
+    Item addedItem(addedItemName, 2, 3, 3, 20);
+
     for (const auto& section : kitchenMasterTest.getSections())
     {
         cout << section.getSectionName() << endl;
     }
 
-    
+
+    kitchenMasterTest.addItem(addedItem, sectionName);
+
+    for (const auto& user : userList.getUsers())
+    {
+        for (const auto& allergy: user.getAllergies())
+        {
+            if (addedItem.getItemName() == allergy)
+            {
+                kitchenMasterTest.removeItem(addedItem.getItemName(), sectionName);
+                cout << "A user of this fridge is allergic to this item.\n";
+            }
+        }
+    }
 
     // specified fridge associated users implementation database
 
