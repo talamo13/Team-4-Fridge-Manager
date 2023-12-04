5716f0a67266c73952aa28f1976bd2aef9c63731
kelsonnguyen <kelsonnguyen007@gmail.com>
2023-12-04 11:07:26 -0800

    removed unnecessary comments

--- main.cpp
+++ main.cpp
@@ -32,37 +32,14 @@ int main()
     FridgesDatabase fridges; // loads database for saved fridges
     Fridge kitchenMasterTest = fridges.getFridges()[0]; // 71360 volume total
     Section sectionTest;
-
+    displayMenu();
     selectChoices(userList, loggedIn, savedItems, fridges, kitchenMasterTest, sectionTest);
-    
-    Item normalItem("normal", 1, 1, 2, 500, "Unknown"); // Item 1: normal, no problem
-    Item allergicItem("Shellfish", 1, 2, 3, 400, "Fish"); // Item 2: a user is allergic
-    Item bigItem("nuclear bomb", 2000, 4000, 2, 3, "Unknown"); // Item 3: item is too big
-    Item allergicBigItem("Shellfish that is nuclear", 2000, 4000, 2, 3, "Fish"); // Item 4: has allergy name 
-    Item allergicSmallItem("Shellfish that is sooo big", 1, 1, 1, 2, "Fish"); // Item 5: has allergy name
-
-    addItemToFridge(userList, kitchenMasterTest, normalItem, sectionTest);
-    addItemToFridge(userList, kitchenMasterTest, allergicItem, sectionTest);
-    addItemToFridge(userList, kitchenMasterTest, bigItem, sectionTest);
-    addItemToFridge(userList, kitchenMasterTest, allergicBigItem, sectionTest);
-    addItemToFridge(userList, kitchenMasterTest, allergicSmallItem, sectionTest);
-    
-    for (const auto& i : kitchenMasterTest.getContents())
-    {
-        cout << i.getItemName() << " ";
-    }
 
     // specified fridge associated users implementation database
 
     // specified fridge items in fridge 
 
     // user class and their info database
-
-
-    // Front-End
-    // displayMenu();
-    // selectChoices();
-
 
     // fridges.saveToFile();
     userList.saveToFile(); // always save to file to update databases
